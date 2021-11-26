181e3702a919a4998509356ceb6d8f7f039cda83
charan-sumasoft <charan.tatu@sumasoft.net>
2021-11-26 16:01:32 +0530

    new change

--- aws-node-http-api-project/handler.js
+++ aws-node-http-api-project/handler.js
@@ -2,8 +2,8 @@
 const { CognitoUserPool, CognitoUser } = require('amazon-cognito-identity-js')
 
 let poolData = {
-    UserPoolId: "ap-south-1_IBopTt4z4", // User Pool Id
-    ClientId: "4m5nid09grkk37m4lb0bmbfm69" // Client Id
+  UserPoolId: "ap-south-1_IBopTt4z4", // User Pool Id
+  ClientId: "4m5nid09grkk37m4lb0bmbfm69" // Client Id
 }
 
 let userPool = new CognitoUserPool(poolData);
@@ -31,16 +31,16 @@ module.exports.hello = async (event) => {
 const aws = require('aws-sdk')
 
 var params = {
-  TableName : "UserData",
+  TableName: "UserData",
   KeySchema: [
-      { AttributeName: "id", KeyType: "HASH" }
+    { AttributeName: "id", KeyType: "HASH" }
   ],
   AttributeDefinitions: [
-      { AttributeName: "id", AttributeType:"S"}
-          ],
+    { AttributeName: "id", AttributeType: "S" }
+  ],
   ProvisionedThroughput: {
-      ReadCapacityUnits: 5, 
-      WriteCapacityUnits: 5
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5
   }
 }
 aws.config.update({
@@ -53,110 +53,110 @@ aws.config.update({
 
 
 var dynamodb = new aws.DynamoDB();
-module.exports.create = async (events)=>{
-  
-    try {
-        let createTable = await dynamodb.createTable(params).promise();
-        return {
-            statusCode: 200 ,
-            body: JSON.stringify({
-                data: createTable,
-                message:"successful",
-                input: events,
-            })
-        }  
-    } catch (error) {
-        return{
-            statusCode: error.statusCode || 500 ,
-            body: JSON.stringify({
-                data: {},
-                message:"Unsuccessful",
-                input: events,
-                errorName : error.name,
-                errorDetails : error
-            })
-        }
+module.exports.create = async (events) => {
+
+  try {
+    let createTable = await dynamodb.createTable(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: createTable,
+        message: "successful",
+        input: events,
+      })
     }
+  } catch (error) {
+    return {
+      statusCode: error.statusCode || 500,
+      body: JSON.stringify({
+        data: {},
+        message: "Unsuccessful",
+        input: events,
+        errorName: error.name,
+        errorDetails: error
+      })
+    }
+  }
 }
 //charan
 
-function otp(userName){
-  return new Promise((resolve,reject)=>{
-    
-      let user = new CognitoUser({
-        Username: userName,
-        Pool: userPool // User Pool
-      })
-      user.forgotPassword({
-          onSuccess: function (data) {
-              // successfully initiated reset password request
-              console.log('CodeDeliveryData from forgotPassword: ');
-              console.log(data);
-              resolve(data)
-          },
-          onFailure: function (err) {
-              console.log(err);
-              reject(err);
-          }
-      })
+function otp(userName) {
+  return new Promise((resolve, reject) => {
+
+    let user = new CognitoUser({
+      Username: userName,
+      Pool: userPool // User Pool
+    })
+    user.forgotPassword({
+      onSuccess: function (data) {
+        // successfully initiated reset password request
+        console.log('CodeDeliveryData from forgotPassword: ');
+        console.log(data);
+        resolve(data)
+      },
+      onFailure: function (err) {
+        console.log(err);
+        reject(err);
+      }
+    })
   })
 }
 
-function confirmNewPassword(otp,password){
-  return new Promise((resolve,reject)=>{
-      user.confirmPassword(otp, password, {
-          onSuccess(data) {
-              // After password is reset
-              console.log("passwaord reset");
-              resolve("password changed successfully")
-          },
-          onFailure(err) {
-              reject(err)
-          },
-      });
+function confirmNewPassword(otp, password) {
+  return new Promise((resolve, reject) => {
+    user.confirmPassword(otp, password, {
+      onSuccess(data) {
+        // After password is reset
+        console.log("passwaord reset");
+        resolve("password changed successfully")
+      },
+      onFailure(err) {
+        reject(err)
+      },
+    });
   })
 }
 
 
-module.exports.getOtp = async (events)=>{
+module.exports.getOtp = async (events) => {
   const userData = JSON.parse(events.body);
-  
+
   try {
-      let code = await otp(userData.email);
-      console.log(code);
-      return {
-          statusCode: 200,
-          body: JSON.stringify({
-              message: "code send"
-          })
-      }
+    let code = await otp(userData.email);
+    console.log(code);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "code send"
+      })
+    }
   } catch (error) {
-      return {
-          statusCode: 400,
-          body: JSON.stringify({
-              message: "code send error"
-          })
-      }
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "code send error"
+      })
+    }
   }
 }
 
-module.exports.newPassWord = async (events)=>{
+module.exports.newPassWord = async (events) => {
   const userData = JSON.parse(events.body);
   try {
-      let code = await confirmNewPassword(userData.otp,userData.password);
-      console.log(code);
-      return {
-          statusCode: 200,
-          body: JSON.stringify({
-              message: "password change succfully"
-          })
-      }
+    let code = await confirmNewPassword(userData.otp, userData.password);
+    console.log(code);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "password change succfully"
+      })
+    }
   } catch (error) {
-      return {
-          statusCode: 400,
-          body: JSON.stringify({
-              message: "code send"
-          })
-      }
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "code send"
+      })
+    }
   }
 }
